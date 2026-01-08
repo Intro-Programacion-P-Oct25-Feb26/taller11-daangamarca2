88c5c14e866b5f25eab7ba9dbe60f1061d9e5a8c
daangamarca2 <arturoangamarca120@gmail.com>
2026-01-08 11:06:12 -0500

    Problema-1

--- ejercicios/Problemas/src/paquete01/Problema1.java
+++ ejercicios/Problemas/src/paquete01/Problema1.java
@@ -20,7 +20,15 @@ public class Problema1 {
     }
 
     public static void imprimirDatos(int[][] datos) {
-
+        String cadena = "";
+        for (int i = 0; i < datos.length; i++){
+            for (int j = 0; j < datos.length; j++){
+                if (datos [i][j] %2 == 0){
+                    cadena = cadena + datos [i][j] + " ";
+                }
+            }
+        }
+        System.out.printf("%s\n", cadena);
     }
 
 }
