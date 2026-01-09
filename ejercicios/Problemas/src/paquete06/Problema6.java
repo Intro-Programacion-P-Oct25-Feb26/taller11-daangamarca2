31e3c4893e76524d22fa6a245b7aa2332b393c44
daangamarca2 <arturoangamarca120@gmail.com>
2026-01-08 21:41:27 -0500

    Problema-6

--- ejercicios/Problemas/src/paquete06/Problema6.java
+++ ejercicios/Problemas/src/paquete06/Problema6.java
@@ -10,5 +10,35 @@ package paquete06;
  * @author reroes
  */
 public class Problema6 {
-    
+
+    public static void main(String[] args) {
+        int[] informacion = {1, 2, 3, 10, 20, 30, 100, 200, 300};
+        double media = obtenerMediaAritmetica(informacion);
+        double desviacion = obtenerDesviacion(informacion, media);
+        obtenerReporte(media, desviacion);
+        
+    }
+
+    public static double obtenerMediaAritmetica(int[] info) {
+        double suma = 0;
+        for (int i = 0; i < info.length; i++) {
+            suma = suma + info[i];
+        }
+        return suma / info.length;
+    }
+
+    public static double obtenerDesviacion(int[] info, double media) {
+        double suma = 0;
+        for (int i = 0; i < info.length; i++) {
+            suma = suma + Math.pow(info[i] - media, 2);
+        }
+        return Math.sqrt(suma / info.length);
+    }
+
+    public static void obtenerReporte(double m, double d) {
+        String mensaje = "";
+        mensaje = mensaje + String.format("Media: %.2f\nDesviacion Estandar: "
+                + "%.2f\n", m, d);
+        System.out.println(mensaje);
+    }
 }
