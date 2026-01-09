6bdfa42ca1c61b85e473bef990d65f0a8694d534
daangamarca2 <arturoangamarca120@gmail.com>
2026-01-08 20:59:23 -0500

    Problema-5

--- ejercicios/Problemas/src/paquete05/Problema5.java
+++ ejercicios/Problemas/src/paquete05/Problema5.java
@@ -10,5 +10,48 @@ package paquete05;
  * @author reroes
  */
 public class Problema5 {
-    
+
+    public static void main(String[] args) {
+        // TODO code application logic here\
+
+        int[][] informacion = {
+            {1, 2, 3},
+            {10, 20, 30},
+            {100, 200, 300}
+        };
+
+        int[][] informacion2 = {
+            {1, 2, 3},
+            {10, 20, 30},
+            {100, 200, 300}
+        };
+
+        int[][] resultado = sumarMatrices(informacion, informacion2);
+
+        imprimirMatriz(resultado);
+    }
+
+    public static int[][] sumarMatrices(int[][] matriz1, int[][] matriz2) {
+        int filas = matriz1.length;
+        int columnas = matriz1[0].length;
+        int[][] suma = new int[filas][columnas];
+
+        for (int i = 0; i < filas; i++) {
+            for (int j = 0; j < columnas; j++) {
+                suma[i][j] = matriz1[i][j] + matriz2[i][j];
+            }
+        }
+        return suma;
+    }
+
+    public static void imprimirMatriz(int[][] matriz) {
+        String mensaje = "";
+        for (int i = 0; i < matriz.length; i++) {
+            mensaje = "";
+            for (int j = 0; j < matriz[i].length; j++) {
+                mensaje = mensaje + String.format("%s\t", matriz[i][j]);
+            }
+            System.out.printf(mensaje + "\n");
+        }
+    }
 }
